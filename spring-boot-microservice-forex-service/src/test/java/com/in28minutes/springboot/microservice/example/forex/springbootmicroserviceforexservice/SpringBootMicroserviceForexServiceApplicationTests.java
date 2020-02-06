7c26ceda74c76996942b26086525cbc7aae6414c
vic3r <victor.rosales@Victors-MacBook-Pro.local>
2020-02-06 17:08:42 -0600

    Add microservices with proxy

---
+++ spring-boot-microservice-forex-service/src/test/java/com/in28minutes/springboot/microservice/example/forex/springbootmicroserviceforexservice/SpringBootMicroserviceForexServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.in28minutes.springboot.microservice.example.forex.springbootmicroserviceforexservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+class SpringBootMicroserviceForexServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
